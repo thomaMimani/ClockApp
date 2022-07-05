339b76567dcfd2e0ae69997d3c7399e83e34e82c
Thoma <hmimani@outlook.com>
2022-07-05 20:46:53 +0200

    Finished cloack functionality WIP

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ let stopwatchBtn = document.querySelector(`.stopwatch`)
 let time = document.querySelector(`.time`)
 let stopWatch = document.querySelector(`.stopwatch-cocntent`)
 
+let realTime = document.querySelector(`.realTime`)
+
 
 
 function clock(){
     time.classList.remove(`hidden`)
     stopWatch.classList.add(`hidden`)
+
+    
 }
+
 
 
 
@@ -17,3 +22,29 @@ function stopwatch(){
     stopWatch.classList.remove(`hidden`)
     time.classList.add(`hidden`)
 }
+
+// Clock funcionality
+
+function currentTime(time){
+    let date = new Date
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+    let session ='AM'
+    
+    if(hh === 12){
+        hh = 12
+    }
+    if(hh > 12){
+        hh = hh-12
+        session= "PM"
+    }
+
+    hh = (hh<10)?`0`+ hh: hh
+    mm = (mm< 10)? `0`+ mm: mm
+    ss = (ss< 10)? `0`+ ss: ss
+
+    time.textContent = `${hh}:${mm}:${ss}:${session}`
+     setTimeout(function(){ currentTime(realTime) }, 1000)
+}
+currentTime(realTime)
