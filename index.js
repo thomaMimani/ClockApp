81bf958e4e1c6a3250ffb0055aca7b7bebb50bd4
Thoma <hmimani@outlook.com>
2022-07-05 20:10:36 +0200

    no message

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function clock(){
+let clockBtn = document.querySelector(`.clock`)
+let stopwatchBtn = document.querySelector(`.stopwatch`)
 
+let time = document.querySelector(`.time`)
+let stopWatch = document.querySelector(`.stopwatch-cocntent`)
+
+
+
+function clock(){
+    time.classList.remove(`hidden`)
+    stopWatch.classList.add(`hidden`)
 }
 
 
 
 function stopwatch(){
-    
+    stopWatch.classList.remove(`hidden`)
+    time.classList.add(`hidden`)
 }
