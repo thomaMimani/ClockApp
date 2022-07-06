6c1cc668c31d81dfbc0f087312d5dc52b7a80026
Thoma <hmimani@outlook.com>
2022-07-06 22:32:39 +0200

    Added stop and reset functionality WIP

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ let stopWatch = document.querySelector(`.stopwatch-cocntent`)
 
 let realTime = document.querySelector(`.realTime`)
 
+let startBtn = document.querySelector(`.start`)
+
 
 
 function clock(){
     time.classList.remove(`hidden`)
     stopWatch.classList.add(`hidden`)
-
+    
     
 }
 
@@ -43,8 +45,69 @@ function currentTime(time){
     hh = (hh<10)?`0`+ hh: hh
     mm = (mm< 10)? `0`+ mm: mm
     ss = (ss< 10)? `0`+ ss: ss
-
+    
     time.textContent = `${hh}:${mm}:${ss}:${session}`
      setTimeout(function(){ currentTime(realTime) }, 1000)
 }
 currentTime(realTime)
+
+//StopWatch starting condition
+let minEl = document.querySelector(`.min`)
+let secEl = document.querySelector(`.sec`)
+let milEl = document.querySelector(`.mil`)
+
+let mil = 0
+let sec = 0 
+let min = 00
+
+minEl.textContent='0'+min
+secEl.textContent=`0`+sec
+milEl.textContent=`0`+mil
+
+
+
+let timerEl = document.querySelector(`.timerEl`)
+
+
+
+
+let timer;
+
+
+function start(){
+    startBtn.disabled=true
+    
+    timer = setInterval(() => {
+    
+        mil++
+    if(mil>99){
+        sec++
+        mil=0
+    }
+    if(sec>59){
+        min=min+1
+        sec=0
+    }
+    
+    minEl.textContent=(min<10)?'0'+min:min
+    secEl.textContent=(sec<10)?`0`+sec:sec
+    milEl.textContent=(mil<10)?`0`+mil:mil
+       }, 10);
+
+}
+
+function stop(){
+    
+    clearInterval(timer)
+}
+
+function reset() {
+    mil = 0
+    sec = 0 
+    min = 00
+
+minEl.textContent='0'+min
+secEl.textContent=`0`+sec
+milEl.textContent=`0`+mil
+
+}
